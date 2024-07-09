08733fdce7e1dda10d085024dbaa30955391cfe1
PavelkoDmitriy <pavelkodmitriy@gmail.com>
2024-07-09 23:48:07 +0300

    Create homework2.py

---
+++ homework2/homework2.py
@@ -0,0 +1,4 @@
+completed = 12
+spent_time = 1.5
+course_name = "Python"
+print("Курс:", course_name + ',', "всего задач: " + str(completed) + ',', "затрачено часов:", str(spent_time) + ',', "среднее время выполнения", 1.5/12, "часа.")
