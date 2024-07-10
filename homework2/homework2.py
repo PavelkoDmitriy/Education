fc6d41cfa3bdcfd0cbf764a07c0f048429868959
PavelkoDmitriy <PavelkoDmitriy@users.noreply.github.com>
2024-07-10 17:32:10 +0300

    Update homework2.py

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,4 +1,5 @@
 completed = 12
 spent_time = 1.5
 course_name = "Python"
-print("Курс:", course_name + ',', "всего задач: " + str(completed) + ',', "затрачено часов:", str(spent_time) + ',', "среднее время выполнения", 1.5/12, "часа.")
+average_time = spend_time / completed
+print("Курс:", course_name + ',', "всего задач: " + str(completed) + ',', "затрачено часов:", str(spent_time) + ',', "среднее время выполнения", average_time, "часа.")
