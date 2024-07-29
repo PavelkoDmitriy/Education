97f4258f2e438a05b07c82d2528d9bfb1ec996ee
PavelkoDmitriy <pavelkodmitriy@gmail.com>
2024-07-29 22:57:41 +0300

    Update module_3_4.py

--- module_3_4/module_3_4.py
+++ module_3_4/module_3_4.py
@@ -1,10 +1,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word.lower() in i.lower():
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
-        elif i.lower() in root_word.lower():
-            same_words.append(i)
+        #elif i.lower() in root_word.lower():
+        #    same_words.append(i)
     return same_words
 
 
