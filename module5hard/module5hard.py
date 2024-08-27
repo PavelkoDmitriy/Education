4ff68f63584ccdb869faf093f1a4974140beffb2
PavelkoDmitriy <pavelkodmitriy@gmail.com>
2024-08-27 23:07:38 +0300

    Create module5hard.py

---
+++ module5hard/module5hard.py
@@ -0,0 +1,61 @@
+class User:
+    def __init__(self):
+        pass
+
+    def nickname(self):
+        pass
+
+    def password(self):
+        pass
+
+    def age(self):
+        pass
+
+
+class Video:
+    def __init__(self):
+        pass
+
+    def title(self):
+        pass
+
+    def duration(self):
+        pass
+
+    def time_now(self):
+        pass
+
+    def adult_mode(self):
+        pass
+
+
+class UrTube:
+    def __init__(self):
+        pass
+
+    def users(self):
+        pass
+
+    def videos(self):
+        pass
+
+    def current_user(self):
+        pass
+
+    def log_in(self):
+        pass
+
+    def register(self):
+        pass
+
+    def log_out(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def get_videos(self):
+        pass
+
+    def watch_video(self):
+        pass
