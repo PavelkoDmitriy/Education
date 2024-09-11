a9abe8d8a01dc8efc3f2bc2d3ba5481f90c72e91
PavelkoDmitriy <PavelkoDmitriy@users.noreply.github.com>
2024-09-11 16:52:07 +0300

    Update module_7_1.py

--- module_7_1/module_7_1.py
+++ module_7_1/module_7_1.py
@@ -6,15 +6,17 @@ class Product:
 
     def __str__(self) -> str:
         return f"{self.name}, {self.weight}, {self.category}"
-
-
+    
 class Shop:
-    __file_name = "products.txt"
+    __file_name = 'products.txt'
+    
     def get_products(self):
-        file = open(self.__class__.__file_name, 'r')
-        file_= file.read()
+        file = open(self.__file_name, 'r')
+        file_ = file.read()
         file.close()
-        return file_
+        print(file_)
 
-s = Shop()
-print(s.get_products)
+
+Shop().get_products()
+
+
