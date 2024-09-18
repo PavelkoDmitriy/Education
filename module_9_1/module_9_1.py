c57e29e55f492e30dab0deb45c120c00bbc5a3a6
PavelkoDmitriy <PavelkoDmitriy@users.noreply.github.com>
2024-09-18 10:38:14 +0300

    Create module_9_1.py

---
+++ module_9_1/module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    res = dict()
+    for i in functions:
+        res[i.__name__] = i(int_list)
+    return res
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
