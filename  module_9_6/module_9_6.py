8cc5332da75c88bb4b1ff7704f5f19748fc1fe6f
PavelkoDmitriy <PavelkoDmitriy@users.noreply.github.com>
2024-09-26 16:44:11 +0300

    Update module_9_6.py

---  module_9_6/module_9_6.py
+++  module_9_6/module_9_6.py
@@ -1,7 +1,7 @@
 def all_variants(text):
-    for length in range(1, len(text) + 1):
-        for start in range(len(text) - length + 1):
-            yield text[start:start + length]
+    for i in range(1, len(text) + 1):
+        for j in range(len(text) - i + 1):
+            yield text[j:j + i]
 
 
 text = "abc"
