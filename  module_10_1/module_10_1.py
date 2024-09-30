e122949ea4bf64aa83e5aeb3c41bb4f90dc600f1
PavelkoDmitriy <PavelkoDmitriy@users.noreply.github.com>
2024-09-30 10:44:35 +0300

    Create module_10_1.py

---
+++  module_10_1/module_10_1.py
@@ -0,0 +1,37 @@
+from time import sleep as delay
+from datetime import datetime
+from threading import Thread
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count+1):
+            file.write(f"Какое-то слово № {i}\n")
+            delay(0.1)
+        print(f"Завершилась запись в файл {file_name}")
+
+
+time_now = datetime.now()
+write_words(10, "example1.txt")
+write_words(30, "example2.txt")
+write_words(200, "example3.txt")
+write_words(100, "example4.txt")
+print(datetime.now() - time_now)
+
+
+thread_1 = Thread(target=write_words, args=(10, "example1.txt"))
+thread_2 = Thread(target=write_words, args=(30, "example2.txt"))
+thread_3 = Thread(target=write_words, args=(200, "example3.txt"))
+thread_4 = Thread(target=write_words, args=(100, "example4.txt"))
+
+time_now = datetime.now()
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+thread_4.join()
+print(datetime.now() - time_now)
