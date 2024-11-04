70c389d942ede1eabea70536962019dae38d8f21
PavelkoDmitriy <pavelkodmitriy@gmail.com>
2024-11-04 13:50:20 +0300

    Update tests_12_2.py

--- tests_12_2/tests_12_2.py
+++ tests_12_2/tests_12_2.py
@@ -1,42 +1,45 @@
 import unittest
-from runner_and_tournament import Tournament, Runner  # Импортируем необходимые классы
+from runner_and_tournament import Tournament, Runner
+
 
 class TournamentTest(unittest.TestCase):
-    all_results = {}
-
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
     def setUp(self):
-        # Создаем бегунов с заданными именами и скоростями
-        self.runner_1 = Runner(name="Усэйн", speed=10)
-        self.runner_2 = Runner(name="Андрей", speed=9)
-        self.runner_3 = Runner(name="Ник", speed=3)
+        self.runner1 = Runner("Усэйн", 10)
+        self.runner2 = Runner("Андрей", 9)
+        self.runner3 = Runner("Ник", 3)
+
+    def test1(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник')
+        self.__class__.all_results['test1'] = results
+
+    def test2(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник')
+        self.__class__.all_results['test2'] = results
+
+    def test3(self):
+        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник')
+        self.__class__.all_results['test3'] = results
 
     @classmethod
     def tearDownClass(cls):
-        # Выводим результаты всех тестов
-        for key in sorted(cls.all_results.keys()):
-            print(f"{key}: {cls.all_results[key]}")
+        # Выводим все результаты тестов
+        for test_name, result in cls.all_results.items():
+            printable_result = {place: runner.name for place, runner in result.items()}
+            print(printable_result)
 
-    def test_race_usain_and_nick(self):
-        tournament = Tournament(distance=90)
-        results = tournament.start(self.runner_1, self.runner_3)
-        self.all_results[max(results.keys())] = results[max(results.keys())]
-        self.assertEqual(results[max(results.keys())], "Ник")
-
-    def test_race_andrey_and_nick(self):
-        tournament = Tournament(distance=90)
-        results = tournament.start([self.runner_2, self.runner_3])
-        self.all_results[max(results.keys())] = results[max(results.keys())]
-        self.assertEqual(results[max(results.keys())], "Ник")
-
-    def test_race_usain_andrey_and_nick(self):
-        tournament = Tournament(distance=90)
-        results = tournament.start([self.runner_1, self.runner_2, self.runner_3])
-        self.all_results[max(results.keys())] = results[max(results.keys())]
-        self.assertEqual(results[max(results.keys())], "Ник")
 
 if __name__ == "__main__":
     unittest.main()
