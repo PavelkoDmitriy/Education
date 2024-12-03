23c069cc5418033381d0513dd9e847841af776c5
PavelkoDmitriy <pavelkodmitriy@gmail.com>
2024-12-03 20:52:53 +0300

    Update module_13_4.py

--- module_13_4/module_13_4.py
+++ module_13_4/module_13_4.py
@@ -9,6 +9,17 @@ api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+@dp.message_handler(commands=['start'])
+async def start(message):
+    #print('Привет! Я бот помогающий твоему здоровью.' )
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messages(messege):
+    #print( 'Введите команду /start, чтобы начать общение.')
+    await messege.answer('Введите команду /start, чтобы начать общение.')
+
 
 class UserState(StatesGroup):
     age = State()
